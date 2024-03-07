8704fb370045246e067c2e3b8ffcdee6f8d44c54
Colson <colsoncalifornication@gmail.com>
2024-03-07 21:00:51 +0545

    handle http states and feedback with redux and render notification to the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { Fragment, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { uiActions } from './store/ui-slice';
+import Notification from './components/UI/Notification';
 
 /*
  * Good way of having our side effects logic in a component and keeping
@@ -11,27 +13,74 @@ import Products from './components/Shop/Products';
  * typically wanna have it when working with Redux
  * */
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   // great thing is useSelector sets up a subscription to Redux, so whenever
   // our Redux store does change, this component function will be reexecuted
   // and we'll get the latest state i.e latest cart in our case
   const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    // POST requests will add new data in a lists of data
-    // sending the PUT requests overrrides the existing cart with
-    // the incoming data
-    fetch('https://redux-cart-77e01-default-rtdb.firebaseio.com/cart.json', {
-      method: 'PUT',
-      body: JSON.stringify(cart),
+    const sendCartData = async () => {
+      // currently sending the requests
+      dispatch(
+        uiActions.showNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending cart data!',
+        }),
+      );
+
+      // POST requests will add new data in a lists of data
+      // sending the PUT requests overrrides the existing cart with
+      // the incoming data
+      const response = await fetch(
+        'https://redux-cart-77e01-default-rtdb.firebaseio.com/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+        },
+      );
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.');
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sent cart data successfully!',
+        }),
+      );
+    };
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data failed!',
+        }),
+      );
     });
-  }, [cart]);
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
