a2af4c9a19fdf47d2f3e52f82a4ec5af7550928e
Colson <colsoncalifornication@gmail.com>
2024-03-07 15:40:28 +0545

    create dummy products and render it on products

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,40 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    price: 600000,
+    title: 'Range Rover',
+    description: 'When networth is more than 2 million dollars',
+  },
+  {
+    id: 'p2',
+    price: 300000,
+    title: 'Toyato Fortuner',
+    description: 'When networth is more than 1 million dollars',
+  },
+  {
+    id: 'p3',
+    price: 6000,
+    title: 'MacBook Pro Powerhouse',
+    description: 'When networth is 100,000',
+  },
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.des}
+          />
+        ))}
       </ul>
     </section>
   );
