b44f44cb96a617cfdd7ce6590d7bb4d9dffc5d08
Colson <colsoncalifornication@gmail.com>
2024-03-07 15:18:15 +0545

    work on the removeItemFromCart action on reducers function of cart slice state

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-createSlice({
+const cartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [],
@@ -27,6 +27,25 @@ createSlice({
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
-    removeItemFromCart() {},
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      // find that item with that id and remove it from the array
+      // we dont need to check if its part of the array we know that it will be part
+      // but we need to find out how many items are in the array
+      const existingItem = state.items.find((item) => item.id === id);
+
+      if (existingItem.quantity === 1) {
+        // remove the item from the array
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        // if greater than 1, reduce the quantity by 1
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
   },
 });
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
